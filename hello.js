5b20b972f864fb093c608ec8b8dcb51b3b2d6606
c-nakashima <c-nakashima@users.noreply.github.com>
2021-03-02 06:30:14 +0000

    deploy: 69406eaa06ea438f41bbec0c9b529cb0acfe6ad8

---
+++ hello.js
@@ -0,0 +1,82 @@
+import { d as defineStanzaElement } from './stanza-element-2447d8cc.js';
+
+async function hello(stanza, params) {
+  stanza.render({
+    template: 'stanza.html.hbs',
+    parameters: {
+      greeting: `Hello, ${params['say-to']}!`
+    }
+  });
+}
+
+var stanzaModule = /*#__PURE__*/Object.freeze({
+  __proto__: null,
+  'default': hello
+});
+
+var metadata = {
+	"@context": {
+	stanza: "http://togostanza.org/resource/stanza#"
+},
+	"@id": "hello",
+	"stanza:label": "Hello",
+	"stanza:definition": "first stanza",
+	"stanza:type": "Stanza",
+	"stanza:display": "Text",
+	"stanza:provider": "togostanza",
+	"stanza:license": "MIT",
+	"stanza:author": "c-nakashima",
+	"stanza:address": "nakashima@penqe.com",
+	"stanza:contributor": [
+],
+	"stanza:created": "2021-03-02",
+	"stanza:updated": "2021-03-02",
+	"stanza:parameter": [
+	{
+		"stanza:key": "say-to",
+		"stanza:example": "world",
+		"stanza:description": "who to say hello to",
+		"stanza:required": false
+	}
+],
+	"stanza:about-link-placement": "bottom-right",
+	"stanza:style": [
+	{
+		"stanza:key": "--greeting-color",
+		"stanza:type": "color",
+		"stanza:default": "#eb7900",
+		"stanza:description": "text color of greeting"
+	},
+	{
+		"stanza:key": "--greeting-align",
+		"stanza:type": "single-choice",
+		"stanza:choice": [
+			"left",
+			"center",
+			"right"
+		],
+		"stanza:default": "center",
+		"stanza:description": "text align of greeting"
+	}
+]
+};
+
+var templates = [
+  ["stanza.html.hbs", {"compiler":[8,">= 4.3.0"],"main":function(container,depth0,helpers,partials,data) {
+    var helper, lookupProperty = container.lookupProperty || function(parent, propertyName) {
+        if (Object.prototype.hasOwnProperty.call(parent, propertyName)) {
+          return parent[propertyName];
+        }
+        return undefined
+    };
+
+  return "<p class=\"greeting\">"
+    + container.escapeExpression(((helper = (helper = lookupProperty(helpers,"greeting") || (depth0 != null ? lookupProperty(depth0,"greeting") : depth0)) != null ? helper : container.hooks.helperMissing),(typeof helper === "function" ? helper.call(depth0 != null ? depth0 : (container.nullContext || {}),{"name":"greeting","hash":{},"data":data,"loc":{"start":{"line":1,"column":20},"end":{"line":1,"column":32}}}) : helper)))
+    + "</p>\n";
+},"useData":true}]
+];
+
+var css = "/*\n\nYou can set up a global style here that is commonly used in each stanza.\n\nExample:\n\nh1 {\n  font-size: 24px;\n}\n\n*/\nmain {\n  padding: 1rem 2rem;\n}\n\np.greeting {\n  margin: 0;\n  font-size: 24px;\n  color: var(--greeting-color);\n  text-align: var(--greeting-align);\n}";
+
+defineStanzaElement({stanzaModule, metadata, templates, css, url: import.meta.url});
+//# sourceMappingURL=hello.js.map
